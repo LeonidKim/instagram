bebc1edc2ce80f0a1ac742456da5bce51d1602a3
LeonidKim <Leonid.kim1368@gmail.com>
2019-05-08 21:01:59 +0600

    .

---
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("0e2020ef2c1e353d5185f1c8b5b95fdf09c4e284")
+            .clientKey("cc25b5306962043b3ba9e4799a469e67cd2159b7")
+            .server("http://3.19.60.155:80/parse/")
+            .build()
+    );
+
+
+    //ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
+
